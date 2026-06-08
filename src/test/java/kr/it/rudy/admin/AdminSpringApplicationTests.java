68dde1da404b7d48af3025cd1db0751ab7d0a0dc
Rudy <idejrdud@gmail.com>
2026-06-08 21:28:11 +0900

    first commit

---
+++ src/test/java/kr/it/rudy/admin/AdminSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
